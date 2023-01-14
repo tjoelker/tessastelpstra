468d55f96f61fa259c808a64f5536aa5842330fc
Jelmer Tjoelker <110289451+tjoelker@users.noreply.github.com>
2023-01-14 18:06:33 +0100

    Added function to scan for content in cases

--- source/view/main.php
+++ source/view/main.php
@@ -1,28 +1,54 @@
 <main>
   <?php
-    $path_cases = __DIR__ . '/..' . '/assets' . '/case' . '/';
+    $path_cases = __DIR__ . "/.." . "/assets" . "/case" . "/";
     $cases = scandir($path_cases);
-    $junk_files = [
-      0 => '.',
-      1 => '..',
-    ];
 
-    function clearJunkFiles(array $junk_array, array $cases_array)
+    function clearJunkFiles(array $folder)
     {
-      foreach ($junk_array as $junk)
+      $junk_files = array(
+        0 => ".",
+        1 => "..",
+      );
+
+      foreach ($junk_files as $junk)
       {
-        $junk_key = array_search($junk, $cases_array);
+        $junk_key = array_search($junk, $folder);
 
         if ($junk_key !== false)
         {
-          unset($cases_array[$junk_key]);
+          unset($folder[$junk_key]);
         }
       }
 
-      return $cases_array;
+      return $folder;
+    }
+
+    function printCases(string $path, array $cases)
+    {
+      foreach ($cases as $case)
+      {
+        $heading = $case;
+        $path_case = $path . "$case" . "/";
+        $case = clearJunkFiles(scandir($path_case));
+
+        print_r("<pre>$heading</pre>");
+
+        printCase($path_case, $case);
+      }
+    }
+
+    function printCase(string $path, array $case)
+    {
+      print_r("<pre>$path</pre>");
+      
+      foreach ($case as $item)
+      {
+        print_r("<pre>$item</pre>");
+      }
     }
   ?>
   <pre>
-    <?php var_dump(clearJunkFiles($junk_files, $cases)); ?>
+    <?php var_dump(clearJunkFiles($cases)); ?>
+    <?php printCases($path_cases, clearJunkFiles($cases)); ?>
   </pre>
 </main>
