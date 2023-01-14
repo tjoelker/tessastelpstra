c406f87424470a37dc85fee4e7bc6f80379982df
Jelmer Tjoelker <110289451+tjoelker@users.noreply.github.com>
2023-01-14 16:35:22 +0100

    Created function to scan case directory

--- source/view/main.php
+++ source/view/main.php
@@ -1,5 +1,28 @@
 <main>
-  <div>
-    <pre>main.php</pre>
-  </div>
+  <?php
+    $path_cases = __DIR__ . '/..' . '/assets' . '/case' . '/';
+    $cases = scandir($path_cases);
+    $junk_files = [
+      0 => '.',
+      1 => '..',
+    ];
+
+    function clearJunkFiles(array $junk_array, array $cases_array)
+    {
+      foreach ($junk_array as $junk)
+      {
+        $junk_key = array_search($junk, $cases_array);
+
+        if ($junk_key !== false)
+        {
+          unset($cases_array[$junk_key]);
+        }
+      }
+
+      return $cases_array;
+    }
+  ?>
+  <pre>
+    <?php var_dump(clearJunkFiles($junk_files, $cases)); ?>
+  </pre>
 </main>
