e35f4a0e25af48434ddbf946ec60888c1b5946fd
Jelmer Tjoelker <110289451+tjoelker@users.noreply.github.com>
2023-02-18 11:40:26 +0100

    Subpage partially completed, more improvements

--- build/view/image.php
+++ build/view/image.php
@@ -1,4 +1,4 @@
 <!-- image.php -->
-<figure class="image-wrapper">
-  <img class="image" alt="" src="<?php print_r($case->path . $image); ?>">
-</figure>
+<picture class="image-wrapper">
+  <img class="image" alt="<?php print_r(trim($image, '.jpg')); ?>" src="<?php print_r($case->path . $image); ?>" draggable="false">
+</picture>
